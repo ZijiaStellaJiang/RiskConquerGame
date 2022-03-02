c33d40b665796d69c5814a65aca701be59b85df5
xw218 <xw218@duke.edu>
2022-03-02 18:11:45 -0500

    add pages

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ./gradlew build || exit 1
-./gradlew cloverAggregateReport || exit 1
+./gradlew cloverAggregateReports || exit 1
 scripts/coverage_summary.sh
 cp -r shared/build/reports/clover/html/* /coverage-out/ || exit 1
-#!cp -r client/build/reports/clover/html/* /coverage-out/ || exit 1
-#!cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r client/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
 
