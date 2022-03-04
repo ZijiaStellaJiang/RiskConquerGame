8ba431b280ce4d6863b3b02c2ea7398fb541ecf0
xw218 <xw218@duke.edu>
2022-03-04 11:33:04 -0500

    add server test

---
+++ server/src/test/java/edu/duke/ece651/group4/risc/server/ServerTest.java
@@ -0,0 +1,14 @@
+package edu.duke.ece651.group4.risc.server;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ServerTest {
+  //TODO: test connection
+  @Test
+  public void test_() {
+
+  }
+
+}
