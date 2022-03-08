63b26fcd901f137f3ff90a32fcf4092dca1ea1f9
zj68 <zj68@duke.edu>
2022-03-07 22:36:45 -0500

    add units in map factory

--- server/src/test/java/edu/duke/ece651/group4/risc/server/V1MapFactoryTest.java
+++ server/src/test/java/edu/duke/ece651/group4/risc/server/V1MapFactoryTest.java
@@ -1,13 +1,23 @@
 package edu.duke.ece651.group4.risc.server;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import edu.duke.ece651.group4.risc.shared.Map;
+import edu.duke.ece651.group4.risc.shared.Territory;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class V1MapFactoryTest {
-  @Test
-  public void test_() {
-
-  }
-
+    @Test
+    public void test_generate_map(){
+        V1MapFactory f = new V1MapFactory();
+        Map<Character> map = f.generateMap();
+        Territory<Character> t = new Territory<>("Oz");
+        assertTrue(map.checkTerritoryExists(t));
+        ArrayList<Territory<Character>> myTerri = map.getMyTerritories();
+        Territory<Character> tn = myTerri.get(0);
+        assertEquals(8,tn.getUnitNumber());
+    }
 }
