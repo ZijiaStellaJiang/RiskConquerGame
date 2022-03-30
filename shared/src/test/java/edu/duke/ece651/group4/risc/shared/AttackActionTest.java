200379c3d474c24a9f64da9b46f3bb9a54bc31e7
xueqian <xh110@duke.edu>
2022-03-30 17:55:46 -0400

    add rule for updating

--- shared/src/test/java/edu/duke/ece651/group4/risc/shared/AttackActionTest.java
+++ shared/src/test/java/edu/duke/ece651/group4/risc/shared/AttackActionTest.java
@@ -12,17 +12,33 @@ public class AttackActionTest {
     @Test
     public void test_attack(){
         Territory<Character> t1 = new Territory<>("t1");
-        
-        ArrayList<Unit<Character>> Units = new ArrayList<>(Collections.nCopies(5, new SimpleUnit<>()));
-        t1.addGroupUnit(Units);
+        Territory<Character> terriN = new Territory<Character>("Narnia");
+        Territory<Character> terriO = new Territory<Character>("Oz");
+        Territory<Character> terriM = new Territory<Character>("Mordor");
+        terriN.addNeigh(terriO);
+        terriO.addNeigh(terriM);
+        ArrayList<Unit<Character>> nUnits = new ArrayList<>(Collections.nCopies(5, new SimpleUnit<>()));
+        terriN.addGroupUnit(nUnits);
+        ArrayList<Unit<Character>> oUnits = new ArrayList<>(Collections.nCopies(2, new SimpleUnit<>()));
+        terriO.addGroupUnit(oUnits);
+        ArrayList<Unit<Character>> mUnits = new ArrayList<>(Collections.nCopies(3, new SimpleUnit<>()));
+        terriM.addGroupUnit(mUnits);
         Map<Character> map = new Map<Character>();
         map.addTerritory(t1);
-        TextPlayer p1 = new TextPlayer("Green", 1000, 1000);
+        map.addTerritory(terriN);
+        map.addTerritory(terriO);
+        map.addTerritory(terriM);
+        TextPlayer p1 = new TextPlayer("Green");
+        TextPlayer p2 = new TextPlayer("Blue");
         p1.addToTerritory(t1);
+        p1.addToTerritory(terriN);
+        p2.addToTerritory(terriO);
+        p1.addToTerritory(terriM);
         map.addPlayer(p1);
-        ActionParser parse1 = new ActionParser("update t1 null 6 0");
-        ActionParser parse2 = new ActionParser("update t1 null 5 0");
-        Action<Character> update = new UpdateAction<>();
+        map.addPlayer(p2);
+        ActionParser parse1 = new ActionParser("attack narnia oz 4");
+        ActionParser parse2 = new ActionParser("attack mordor oz 1");
+        Action<Character> attack_move = new MoveAction<>(false);
         attack_move.doAction(parse1,map,p1);
         attack_move.doAction(parse2,map,p1);
         assertEquals(1,terriN.getUnitNumber());
