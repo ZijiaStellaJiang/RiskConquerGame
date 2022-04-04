71b2fc67e9be1b5e0e7bffc3e970de090e8acd7e
Zijia Jiang <zj68@duke.edu>
2022-04-03 22:10:57 -0400

    modify factory

--- shared/src/test/java/edu/duke/ece651/group4/risc/shared/UpdateActionTest.java
+++ shared/src/test/java/edu/duke/ece651/group4/risc/shared/UpdateActionTest.java
@@ -10,12 +10,15 @@ import org.junit.jupiter.api.Test;
 public class UpdateActionTest {
   @Test
   public void test_update() {
+    Territory<Character> t_test = new Territory<>("test",2,2,2);
     Territory<Character> t1 = new Territory<>("t1", 100, 1000, 1000);
     ArrayList<Unit<Character>> Units = new ArrayList<>(Collections.nCopies(5, new SimpleUnit<>()));
     t1.addGroupUnit(Units);
     Map<Character> map = new Map<Character>();
+    map.addTerritory(t_test);
     map.addTerritory(t1);
     TextPlayer p1 = new TextPlayer("Green");
+    p1.addToTerritory(t_test);
     p1.addToTerritory(t1);
     map.addPlayer(p1);
     String result;
