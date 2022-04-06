529592cd2ba9f47ad1bb67a1aaecf3e37c2b4b15
Zijia Jiang <zj68@duke.edu>
2022-04-05 20:37:09 -0400

    get display info

--- client/src/main/java/edu/duke/ece651/group4/risc/client/MapTextView.java
+++ client/src/main/java/edu/duke/ece651/group4/risc/client/MapTextView.java
@@ -36,55 +36,62 @@ public class MapTextView implements View{
 
     @Override
     public void displayPlayerMsg(int id) {
-        String player_name = toDisplay.getPlayerName(id);
-        if(player_name==null){
-            throw new IllegalArgumentException("This is an invalid player id!");
-        }
-        StringBuilder sb = new StringBuilder("You are the " + player_name + " player.\n");
-        Player<Character> thisPlayer = toDisplay.getPlayer(id);
-        ArrayList<String> lose = thisPlayer.getLoseTerritories();
-        ArrayList<String> win = thisPlayer.getWinTerritories();
-        if(lose.size()!=0 || win.size()!=0){
-            sb.append("In the last round,\n");
-            if(lose.size()!=0){
-                sb.append("You lose ");
-                sb.append(makeResultInfo(lose));
-            }
-            if(win.size()!=0){
-                sb.append("You win ");
-                sb.append(makeResultInfo(win));
-            }
-        }
+        String msg = getRoundPlayerMsg(id);
         if(toDisplay.getLoserId()==null){
-            sb.append("What would you like to do?\n");
-            sb.append("  Move <Source> <Destination> <number>\n  Attack <Source> <Destination> <number>\n  Done\n\n");
+            msg = msg+"What would you like to do?\n";
+            msg = msg+"  Move <Source> <Destination> <number>\n  Attack <Source> <Destination> <number>\n  Done\n\n";
         }
-        out.print(sb);
+        out.print(msg);
+//        String player_name = toDisplay.getPlayerName(id);
+//        if(player_name==null){
+//            throw new IllegalArgumentException("This is an invalid player id!");
+//        }
+//        StringBuilder sb = new StringBuilder("You are the " + player_name + " player.\n");
+//        Player<Character> thisPlayer = toDisplay.getPlayer(id);
+//        ArrayList<String> lose = thisPlayer.getLoseTerritories();
+//        ArrayList<String> win = thisPlayer.getWinTerritories();
+//        if(lose.size()!=0 || win.size()!=0){
+//            sb.append("In the last round,\n");
+//            if(lose.size()!=0){
+//                sb.append("You lose ");
+//                sb.append(makeResultInfo(lose));
+//            }
+//            if(win.size()!=0){
+//                sb.append("You win ");
+//                sb.append(makeResultInfo(win));
+//            }
+//        }
+//        if(toDisplay.getLoserId()==null){
+//            sb.append("What would you like to do?\n");
+//            sb.append("  Move <Source> <Destination> <number>\n  Attack <Source> <Destination> <number>\n  Done\n\n");
+//        }
+//        out.print(sb);
     }
 
     @Override
     public void displayVictoryMsg(int id){
-        if(toDisplay.getLoserId()==null){
-            throw new IllegalArgumentException("No one wins yet! Can not use this function here!");
-        }
-        // if this is the loser id
-        if(toDisplay.getLoserId().equals(id)){
-            String winnerName = "";
-            for (int i=0; i<toDisplay.getMyPlayers().size(); i++){
-                if(id!=i){
-                    //find the winner's id
-                    winnerName = toDisplay.getPlayer(i).getName();
-                    break;
-                }
-            }
-            //if(!winnerName.equals("")){
-            out.print("You lose!\n"+winnerName+" is the winner.\nGood luck next time!\n");
-            //}
-        }
-        //else this is the winner id
-        else {
-            out.print("You win!\nCongratulations!\n");
-        }
+        out.print(getVictoryMsg(id));
+//        if(toDisplay.getLoserId()==null){
+//            throw new IllegalArgumentException("No one wins yet! Can not use this function here!");
+//        }
+//        // if this is the loser id
+//        if(toDisplay.getLoserId().equals(id)){
+//            String winnerName = "";
+//            for (int i=0; i<toDisplay.getMyPlayers().size(); i++){
+//                if(id!=i){
+//                    //find the winner's id
+//                    winnerName = toDisplay.getPlayer(i).getName();
+//                    break;
+//                }
+//            }
+//            //if(!winnerName.equals("")){
+//            out.print("You lose!\n"+winnerName+" is the winner.\nGood luck next time!\n");
+//            //}
+//        }
+//        //else this is the winner id
+//        else {
+//            out.print("You win!\nCongratulations!\n");
+//        }
     }
 
     public String displayTerritoryInfo(Territory<Character> toDisplay){
@@ -110,6 +117,55 @@ public class MapTextView implements View{
         sb.append("\n   Level 6 : ");
         sb.append(toDisplay.getLevelUnitNum(6));
         return sb.toString();
+    }
+
+    public String getRoundPlayerMsg(int id){
+        String player_name = toDisplay.getPlayerName(id);
+        if(player_name==null){
+            throw new IllegalArgumentException("This is an invalid player id!");
+        }
+        StringBuilder sb = new StringBuilder("You are the " + player_name + " player.\n");
+        Player<Character> thisPlayer = toDisplay.getPlayer(id);
+        ArrayList<String> lose = thisPlayer.getLoseTerritories();
+        ArrayList<String> win = thisPlayer.getWinTerritories();
+        if(lose.size()!=0 || win.size()!=0){
+            sb.append("In the last round,\n");
+            if(lose.size()!=0){
+                sb.append("You lose ");
+                sb.append(makeResultInfo(lose));
+            }
+            if(win.size()!=0){
+                sb.append("You win ");
+                sb.append(makeResultInfo(win));
+            }
+        }
+        return sb.toString();
+    }
+
+    public String getVictoryMsg(int id){
+        if(toDisplay.getLoserId()==null){
+            throw new IllegalArgumentException("No one wins yet! Can not use this function here!");
+        }
+        // if this is the loser id
+        if(toDisplay.getLoserId().equals(id)){
+            String winnerName = "";
+            for (int i=0; i<toDisplay.getMyPlayers().size(); i++){
+                if(id!=i){
+                    //find the winner's id
+                    winnerName = toDisplay.getPlayer(i).getName();
+                    break;
+                }
+            }
+            return "You lose!\n"+winnerName+" is the winner.\nGood luck next time!\n";
+            //if(!winnerName.equals("")){
+            //out.print("You lose!\n"+winnerName+" is the winner.\nGood luck next time!\n");
+            //}
+        }
+        //else this is the winner id
+        else {
+            return "You win!\nCongratulations!\n";
+            //out.print("You win!\nCongratulations!\n");
+        }
     }
 
     protected String makePlayerInfo(Player<Character> p){
