255f7633d78fb4099818f2281f27d43fd5dfc06d
xw218 <xw218@duke.edu>
2022-04-14 13:06:41 -0400

    update start page

--- client/src/main/java/edu/duke/ece651/group4/risc/controller/StartPageController.java
+++ client/src/main/java/edu/duke/ece651/group4/risc/controller/StartPageController.java
@@ -4,6 +4,7 @@ import edu.duke.ece651.group4.risc.client.SelectServerView;
 import javafx.fxml.FXML;
 
 import javafx.event.ActionEvent;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.FontSmoothingType;
@@ -19,18 +20,19 @@ public class StartPageController {
     Button start_btn;
     @FXML
     Button exit_btn;
-    @FXML
-    Text title;
+//    @FXML
+//    Text title;
     /**
      * initialize the controller
      */
     @FXML
     public void initialize(){
-        title.setFontSmoothingType(FontSmoothingType.LCD);
-        exit_btn.addEventHandler(MouseEvent.MOUSE_ENTERED, e->exit_btn.setStyle("-fx-border-color: blue;-fx-background-color: transparent;-fx-text-fill: black"));
-        exit_btn.addEventHandler(MouseEvent.MOUSE_EXITED,e->exit_btn.setStyle("-fx-border-color: black;-fx-background-color: transparent;-fx-text-fill: black"));
-        start_btn.addEventHandler(MouseEvent.MOUSE_ENTERED, e->start_btn.setStyle("-fx-border-color: blue;-fx-background-color: transparent;-fx-text-fill: black"));
-        start_btn.addEventHandler(MouseEvent.MOUSE_EXITED,e->start_btn.setStyle("-fx-border-color: black;-fx-background-color: transparent;-fx-text-fill: black"));
+//        title.setFontSmoothingType(FontSmoothingType.LCD);
+        exit_btn.addEventHandler(MouseEvent.MOUSE_ENTERED, e->exit_btn.setCursor(Cursor.HAND));
+        //exit_btn.addEventHandler(MouseEvent.MOUSE_EXITED,e->exit_btn.setStyle("-fx-border-color: black;-fx-background-color: transparent;-fx-text-fill: black"));
+
+        start_btn.addEventHandler(MouseEvent.MOUSE_ENTERED, e->start_btn.setCursor(Cursor.HAND));
+        //start_btn.addEventHandler(MouseEvent.MOUSE_EXITED,e->start_btn.setStyle("-fx-border-color: black;-fx-background-color: transparent;-fx-text-fill: black"));
     }
     /**
      * Deal with exit and start botton
